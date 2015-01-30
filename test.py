f870bec94ab27c4dc111948d367cf5e3d145b4c0
Alon Zakai <alonzakai@gmail.com>
2015-01-30 13:38:24 -0800

    work

--- test.py
+++ test.py
@@ -2,15 +2,27 @@ import random
 
 # Vars
 
-class SimpleVar:
+class Var:
+  def __init__(self):
+    self.clear()
+
+  def clear(self):
+    self.value = None
+
+class SimpleVar(Var):
   def __init__(self, pfail):
+    self.clear()
+
     self.pfail = pfail
 
   def sample(self):
+    if self.value is not None: return
     self.value = 1 if random.random() < self.pfail else 0
 
-class ComboVar:
+class ComboVar(Var):
   def __init__(self, sources, pfail):
+    self.clear()
+
     self.sources = sources
 
     sources_pass = 1
@@ -21,18 +33,30 @@ class ComboVar:
     self.factor = pfail / sources_fail
 
   def sample(self):
+    for source in self.sources: source.sample()
     source_fails = 1 in [source.value for source in self.sources]
     self.value = 1 if source_fails and random.random() < self.factor else 0
 
 # Problems
 
 class Problem:
-  def __init__(self):
-    self.vars = [SimpleVar(0.5), SimpleVar(0.5)]
-    eps = 0.001
-    self.vars.append(ComboVar(self.vars[:], 0.5 + eps))
+  def __init__(self, smart):
+    base = 0.01
+    good = []
+    bad = []
+    for i in range(10):
+      x = SimpleVar(base)
+      y = SimpleVar(base)
+      z = ComboVar([x, y], base * 1.0001)
+      good += [x, y]
+      bad += [z]
+    if smart:
+      self.vars = good + bad
+    else:
+      self.vars = bad + good
 
   def sample(self):
+    for var in self.vars: var.clear()
     for var in self.vars: var.sample()
     return [var.value for var in self.vars]
 
@@ -48,11 +72,31 @@ class Analyzer:
     for s in self.samples:
       for i in range(len(sums)):
         sums[i] += s[i]
-    means = map(lambda s: s/float(len(self.samples)), sums)
-    print means
+    self.means = map(lambda s: s/float(len(self.samples)), sums)
+    #print self.means
+
+  def estimate(self):
+    l = 0
+    for sample in self.samples:
+      if 1 in sample:
+        #l += sample.index(1) + 1
+        l += (sample.index(1) + 1)**2
+    self.loss = l/float(len(self.samples))
+    return self.loss
 
 # Main
 
-a = Analyzer(Problem(), 10000)
+N = 100000
+
+a = Analyzer(Problem(True), N)
 a.analyze()
+smart = a.estimate()
+print 'smart:', smart
+
+a = Analyzer(Problem(False), N)
+a.analyze()
+dumb = a.estimate()
+print 'dumb:', dumb
+
+print 'factor:', dumb/smart
 
